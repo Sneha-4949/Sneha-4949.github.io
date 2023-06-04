cc7ea175f6a0f0074062f178a0f253f8f484fb7a
Sneha-4949 <snehadutta10899@gmail.com>
2023-06-04 21:23:01 +0530

    modified

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ function opentab(tabname)
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+var sidemenu=document.getElementById("sidemenu");
+function openmenu()
+{
+    sidemenu.style.top="0";
+}
+function closemenu()
+{
+    sidemenu.style.right="-200px";
+}
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwxqoI48-ka19jRuDDsIeQ3W6nDIWWBnd-bABy8PWyzzlCQW8dHEF9KrJdH_5WUiTp-/exec';
+  const form = document.forms['submit-to-google-sheet'];
+  const msg= document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent successfully!";
+        setTimeout(function(){
+            msg.innerHTML="";
+        },5000);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
